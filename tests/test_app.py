2a6dd2b951d01c09f50ca69bdccb751083caa03f
adityarj-pazuzu <aditya.jambhalikar001@gmail.com>
2025-03-09 13:35:31 +0530

    tests pylint fixes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+"""
+This module contains tests for the Flask app.
+"""
+
 import pytest
 from app import app  # Import the Flask app
 
@@ -5,15 +9,15 @@ from app import app  # Import the Flask app
 @pytest.fixture
 def client():
     """
-    Fixture to set up the test client for Flask app
+    Fixture to set up the test client for Flask app.
     """
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as flask_client:
+        yield flask_client
 
 
 def test_hello_world(client):
     """
-    Test the '/' route to ensure it returns the correct response
+    Test the '/' route to ensure it returns the correct response.
     """
     response = client.get("/")
     assert response.status_code == 200
