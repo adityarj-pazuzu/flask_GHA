5241a97d667443d47e4b819cf3c1ea6c70894cc2
adityarj-pazuzu <aditya.jambhalikar001@gmail.com>
2025-03-09 10:31:25 +0530

    added pylint

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+"""A simple Hello world code in Flask"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+MSG = "Hello, World"
+
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    """
+    This function prints the msg as defined
+    :return: msg string
+    """
+    return f"{MSG}"
 
 
 if __name__ == "__main__":
